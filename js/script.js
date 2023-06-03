ecf4e752fa1adfa3a6be465c83203d05cfd2b3f2
Susan Delgado <susanndelgado@gmail.com>
2023-06-02 21:11:14 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// /**
+//   * @desc Main Javascript file contains user interactions
+//   * @author Susan Delgado sdelgado@crf.org
+//   * @required jquery
+// */
+// $(function() {
+
+
+// });
+
+
+   
